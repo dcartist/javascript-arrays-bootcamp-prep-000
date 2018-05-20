c1ef4ac7ab9ae5885f2ad7d956d9a6db7fcc054b
dcartist <pbannerman13@gmail.com>
2018-05-20 17:42:22 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -33,7 +33,7 @@ function destructivelyRemoveElementFromBeginningOfArray (x){
 }
 //issues beginning
 function removeElementFromBeginningOfArray(x){
-  x.slice()
+  x.slice(0)
 }
 //issues ending
 function destructivelyRemoveElementFromEndOfArray (x){
